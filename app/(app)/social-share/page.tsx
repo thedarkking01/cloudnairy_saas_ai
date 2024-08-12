1c3c4bf5c99aa1958552fab3cee2df25f519aaa6
sourabh singh bais <sourabhsinghbais52@gmail.com>
2024-08-13 00:30:33 +0530

    setup authentication

---
+++ app/(app)/social-share/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function social_share() {
+  return (
+    <div>social_share</div>
+  )
+}
+
+export default social_share
---
+++ app/(app)/video-upload/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function video_uplaod() {
+  return (
+    <div>video_uplaod</div>
+  )
+}
+
+export default video_uplaod
